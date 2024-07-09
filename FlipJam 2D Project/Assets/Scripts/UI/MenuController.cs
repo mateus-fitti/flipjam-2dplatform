9ef96e77596236367d9e674d469d40751e5ab66b
Mateus Fittipaldi <mateusfitti@gmail.com>
2024-07-09 16:49:26 -0300

    Added TMP Essentials + Minor Menu Update
    - Cenas adicionadas ao Build Setting
    - Mudança de cena de MenuScene para SampleScene
    - TMP atualizado para 3.0.9

--- FlipJam 2D Project/Assets/Scripts/UI/MenuController.cs
+++ FlipJam 2D Project/Assets/Scripts/UI/MenuController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,5 +19,10 @@ public class MenuController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnSceneChange(String sceneName)
+    {
+        GameController.instance.OnSceneChange(sceneName);
     }
 }
