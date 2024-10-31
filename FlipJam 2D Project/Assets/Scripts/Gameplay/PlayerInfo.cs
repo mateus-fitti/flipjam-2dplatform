23895e75091202d7f16eddf3346c7870b851b36c
Eduardo Santos <94094011+Vanleef@users.noreply.github.com>
2024-10-31 05:36:10 -0300

    Revert "Merge branch 'shooting_system' into input_system"
    This reverts commit f1b7ca39ec53cbde74af152d7af2c6160575610c, reversing
    changes made to 5e49db72f7f9a1f276a4e692a97883339ce9ab4b.

--- FlipJam 2D Project/Assets/Scripts/Gameplay/PlayerInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerInfo : MonoBehaviour
-{
-    public int playerNumber = 1;
-}
--- FlipJam 2D Project/Assets/Scripts/Gameplay/PlayerShoot.cs
+++ FlipJam 2D Project/Assets/Scripts/Gameplay/PlayerShoot.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class PlayerShoot : MonoBehaviour
 {
     private Animator animator;
+    private PlayerInput playerInput;
     private PlayerMovement playerMovement; // Reference to PlayerMovement script
 
     public GameObject bulletPrefab;
@@ -25,19 +26,6 @@ public class PlayerShoot : MonoBehaviour
     public bool isAiming = false;
     private Vector2 aimDirection;
     private Quaternion aimRotation; // Rotation based on aim direction
-
-    public PlayerUpgrade playerUpgrade;
-    public PlayerInfo playerInfo;
-    public PlayerInput playerInput;
-
-    public GameObject bulletPrefab;
-
-    public float projectileSpawnOffset = 0.0f;
-    public float projectileMultiDistance = 0.1f;
-
-    private bool canShoot = true; // Variable to track if the player can shoot
-    private Vector2 currentMove = Vector2.zero;
-
 
     void Awake()
     {
@@ -78,8 +66,6 @@ public class PlayerShoot : MonoBehaviour
         {
             StopAimingAndFire();
         }
-        //        if (playerInput.actions["Fire1"].WasReleasedThisFrame()) Fire(); //this ugly thing is back because we want to fire upon button release
-
     }
 
     private void StartAiming()
@@ -208,56 +194,14 @@ public class PlayerShoot : MonoBehaviour
         canShoot = false; // Set canShoot to false after firing
         StartCoroutine(Reload(reloadTime)); // Start the reload coroutine with the reload time from the bullet script
     }
-    
-    // private void Fire(){
-    //  Projectile bulletScript = bulletPrefab.GetComponent<Projectile>();
-    //     if (bulletScript == null)
-    //     {
-    //         Debug.LogError("Prefab does not have a Projectile script attached.");
-    //         return;
-    //     }
 
-    //     if (!canShoot) return;
-
-    //     Vector3 direction;
-    //     if (currentMove == Vector2.zero)
-    //     {
-    //         if (GetComponent<PlayerMovement>().IsFacingRight)
-    //             direction = Vector3.right;
-    //         else
-    //             direction = Vector3.left;
-    //     }
-    //     else direction = new Vector3(currentMove.x, currentMove.y, 0);
-
-    //     Vector3 dirParallel = new Vector3(-direction.y, direction.x);
-
-    //     int multishotCount = playerUpgrade.GetMultishotCount();
-    //     for (int i = 0; i < multishotCount; i++)
-    //     {
-    //         Vector3 spawnPosition = this.transform.position
-    //             + direction * projectileSpawnOffset
-    //             + dirParallel * ( projectileMultiDistance * (i - (multishotCount-1)/2));
-            
-    //         GameObject projectileInstance = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);
-    //         Projectile projectileComp = projectileInstance.GetComponent<Projectile>();
-    //         projectileComp.SetProjectile(
-    //             direction,
-    //             playerUpgrade.GetProjectileSpeed(),
-    //             playerUpgrade.GetProjectileLifetime(),
-    //             playerInfo.playerNumber
-    //             );
-    //     }
-
-    //     canShoot = false;
-    //     StartCoroutine(Reload(playerUpgrade.GetCooldown()));
-    // }
     private IEnumerator ReenableCollision(Collider2D projectileCollider, Collider2D playerCollider)
     {
         yield return new WaitForSeconds(1f); // Wait for a short delay
         Physics2D.IgnoreCollision(projectileCollider, playerCollider, false); // Re-enable collision
     }
 
-    IEnumerator Reload(float reloadTime)
+    private IEnumerator Reload(float reloadTime)
     {
         yield return new WaitForSeconds(reloadTime); // Wait for the specified reload time
         canShoot = true; // Set canShoot to true after the reload time
